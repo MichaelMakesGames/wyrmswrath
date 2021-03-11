218b7a3cf2b434576ab2e2dd1fa6d6ec877bee71
Michael Moore <mscottmoore@protonmail.com>
2021-03-11 14:28:12 -0600

    Fix tippy styles

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,11 @@ import ReactDOM from "react-dom";
 import { IntlProvider } from "react-intl";
 import Modal from "react-modal";
 import { Provider } from "react-redux";
+import "tippy.js/dist/tippy.css";
 import "./assets/style.css";
 import Game from "./components/Game";
-import store from "./state/store";
 import messages from "./messages";
+import store from "./state/store";
 
 const app = (
   <IntlProvider messages={messages} locale="en" defaultLocale="en">
