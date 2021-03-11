1ea69ffd401f01f4fc6f1f3b28c9998f538f90d6
Michael Moore <mscottmoore@protonmail.com>
2021-03-11 14:40:10 -0600

    Fall back to localStorage if idb fails

--- src/lib/gameSave.ts
+++ src/lib/gameSave.ts
@@ -1,15 +1,40 @@
+/* global localStorage */
 import { set, get } from "idb-keyval";
 import { RawState } from "~types";
 import WrappedState from "~types/WrappedState";
 
+const SAVE_KEY = "serpentine-save";
 export function save(state: RawState): void {
-  set("save", state);
+  try {
+    set(SAVE_KEY, state);
+  } catch {
+    console.info("falling back to localStorage to save");
+    localStorage.setItem(SAVE_KEY, toLocalStorage(state));
+  }
 }
 
 export function load(): Promise<RawState | undefined> {
-  return get("save");
+  try {
+    return get(SAVE_KEY);
+  } catch {
+    console.info("falling back to localStorage to load");
+    const item = localStorage.getItem(SAVE_KEY);
+    return Promise.resolve(item ? fromLocalStorage(item) : undefined);
+  }
 }
 
 export function prepareStateAndSave(state: WrappedState) {
   save(state.raw);
+}
+
+function toLocalStorage(state: RawState): string {
+  return JSON.stringify({
+    ...state,
+    entitiesWithComps: {},
+    entitiesAtPosition: {},
+  });
+}
+
+function fromLocalStorage(item: string): RawState {
+  return JSON.parse(item);
 }
